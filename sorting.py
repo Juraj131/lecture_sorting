de5de5aa812f073572714b8f3b5e814ac2728b02
Zuzana Nova <z.nova@vut.cz>
2023-04-17 09:33:38 +0200

    done2

--- sorting.py
+++ sorting.py
@@ -22,11 +22,30 @@ def read_data(file_name):
                     data[header].append(int(value))
     return data
 
+def selection_sort(stlpec):
+    for i in range(len(stlpec)):
+        min_num = i
+        for j in range(i + 1, len(stlpec)):
+            if stlpec[j] < stlpec[min_num]:
+                min_num = j
+        stlpec[i], stlpec[min_num] = stlpec[min_num], stlpec[i]
+    return stlpec
+
+    #i = -1
+    #min_number = min(stlpec)
+   # for cislo in stlpec:
+      #  i += 1
+       # if cislo
+
+
+
+
 
 def main():
     my_data = read_data('numbers.csv')
     print(my_data['series_1'])
-
+    stlpec = selection_sort(selection_sort(my_data['series_1']))
+    print(stlpec)
 
 if __name__ == '__main__':
     main()
